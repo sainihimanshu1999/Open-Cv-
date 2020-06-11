23ac9ef49ca4aa028dc26b4aadaf9a19b1dc7245
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-06-11 16:33:22 +0530

    ch3&4 done

--- Chapter3.py
+++ Chapter3.py
@@ -5,9 +5,11 @@ img = cv2.imread('Resources/lambo.png')
 print(img.shape)
 
 imgResize = cv2.resize(img, (300, 200))
+imgCropped = img[0:200, 200:500]
 
 cv2.imshow("Image", img)
 cv2.imshow("Image Resize", imgResize)
+cv2.imshow("Image Croppped", imgCropped)
 
 
 cv2.waitKey(0)
